7488183f6cc21bfe08bc53a0a62bafe2e27111a1
John Sullivan <john.t.sullivan@gmail.com>
2015-01-05 17:01:59 -0500

    Properly parses and separates out code and comments, should break when comment chars are in a triple quote string.

--- src/main/scala/me/grison/scalocco/CommentProcessor.scala
+++ src/main/scala/me/grison/scalocco/CommentProcessor.scala
@@ -5,26 +5,30 @@ import scala.util.parsing.combinator.RegexParsers
 
 object CommentParser extends RegexParsers {
 
-  override protected val whiteSpace = " |\t".r
+  val any:Parser[String] = "(?s)(.+?)(?=//|/\\*|\\z)".r("contents")
+  val stringDelim:Parser[String] = "\"|\"\"\"".r
+  val string = stringDelim ~ any ~ stringDelim ^^ {t =>  t._1._1 + t._1._2 + t._2}
 
-  val beginMultilineCommentMarker:Parser[String] = """/\*\*?""".r
-  val continueMultilineComment:Parser[String] = """\*""".r
-  val endMultilineCommentMarker:Parser[String] = """\*/""".r
-  val singleLineCommentMarker:Parser[String] = """//""".r
-  val singleLineCommentText:Parser[String] = "(.*)(?=(\n|$))".r
-  val text:Parser[String] = "(.*)(?=\\*/)".r
-  val newline:Parser[String] = "\n"
+  val multilineCommentRegex:Parser[String] = """(?s)/\*(.*?)\*/""".r("commentString")
+  val singlelineCommentRegex:Parser[String] = """(?m)//(.*)$""".r("commentString")
+  val scaladocRegex:Parser[String] = """(?s)/\*\*(.*?)\*/""".r("commentString")
 
-  val multilineCommentText:Parser[String] = beginMultilineCommentMarker ~> text <~ endMultilineCommentMarker
-  val singlelineComment:Parser[String] = singleLineCommentMarker ~> singleLineCommentText
+  val eof:Parser[String] = """\z""".r
+  val comment = scaladocRegex | multilineCommentRegex | singlelineCommentRegex
+  val code = any <~ guard(comment | eof)
+
+  //val multilineCommentText:Parser[String] = beginMultilineCommentMarker ~> text <~ endMultilineCommentMarker
+  //val singlelineComment:Parser[String] = singleLineCommentMarker ~> singleLineCommentText
   //val comment = singlelineCommentText | multilineCommentText
   //val code = not(comment)
 
   //val doc = (code | comment).*
+  val doc = (code | comment).+
 
   def main(args:Array[String]) {
-    val sourceText = Source.fromFile(args(0)).mkString("\n")
-
+    val sourceText = Source.fromFile(args(0)).mkString
+    //println(sourceText)
+    val res = parseAll((comment.* ~ code).+ <~ eof, sourceText)
     //val res = parseAll(doc, sourceText)
     println(res)
   }
